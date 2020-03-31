aa2404e91ed172e36a90b12690de205f41122bbe
Corinna Fabre <corinna.fabre@gmail.com>
2020-03-31 14:42:34 -0700

    Tried and removed switch.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,74 @@
+
+const math_options = ['add', '+', 'subtract', '-', 'multiply', '*', 'divide', '/', 'exponentiation', '**', 'modulo', '%']
+
+//TASKED WITH: The program should have support for these four operations: addition, subtraction, multiplication, and division.
+//TASKED WITH: The program should accept both the name and the symbol for each possible operation.
+const calculate = function calculate(input) {
+  confirmNumber(input);
+  confirmOperator(input);
+
+  if(input.operation === 'add' || input.operation === '+') {
+    return add(input);
+  }
+  else if(input.operation === 'subtract' || input.operation === '-')  {
+    return subtract(input);
+  }
+  else if(input.operation === 'multiply' || input.operation === '*') {
+    return multiply(input);
+  }
+  else if(input.operation === 'divide' || input.operation === '/') {
+    //TASKED WITH:The program handles divide when attempting to divide by zero
+    if(input.num2 === 0) {
+      console.log('Error: Cannot divide by 0.')
+    }
+    else {
+      return divide(input);
+    }
+  }
+  else if(input.operation === 'modulo' || input.operation === '%') {
+    return modulo(input);
+  }
+}
+
+//TASKED WITH: The program adds numbers accurately with both add and +
+const add = (input) => { console.log(exampleAdditionInput.num1 + exampleAdditionInput.num2); }
+
+//TASKED WITH:The program subtracts numbers accurately with both subtract and -
+const subtract = (input) => { console.log(exampleAdditionInput.num1 - exampleAdditionInput.num2); }
+
+//TASKED WITH:The program adds numbers accurately with both multiply and *
+const multiply = (input) => { console.log(exampleAdditionInput.num1 * exampleAdditionInput.num2); }
+
+//TASKED WITH:The program adds numbers accurately with both divide and /
+const divide = (input) => { console.log(exampleAdditionInput.num1 / exampleAdditionInput.num2); }
+
+//OPTIONAL ENHANCEMENT: Add support for the modulo operator (10 % 3 = 1).
+const modulo = (input) => { console.log(exampleAdditionInput.num1 % exampleAdditionInput.num2); }
+
+//TASKED WITH: The program also needs to handle erroneous operators
+//TASKED WITH:The program handles erroneous input. For example the user might enter clown when asked to enter a number
+
+const confirmOperator = function confirmOperator(input){
+  if(!math_options.includes(input.operation)) {
+    console.log('Error: Please provide a valid operation. \n Provided:', input.operation)
+  }
+}
+
+const confirmNumber = function confirmNumber(input) {
+  if(isNaN(input.num1)) {
+    console.log('Error: Please provide a valid number. \n Provided:', input.num1)
+  }
+  if(isNaN(input.num2)) {
+    console.log('Error: Please provide a valid number. \n Provided:', input.num2)
+  }
+}
+
+//OPTIONAL ENHANCEMENT: Print out the formula in addition to the result, i.e. 2 + 2 = 4
+//OPTIONAL ENHANCEMENT: Add support for computing exponents (2^4 = 2 * 2 * 2 * 2 = 16)
+//OPTIONAL ENHANCEMENT: Make your program know when it needs to return an integer versus a float.
+//OPTIONAL ENHANCEMENT: Add support for parentheticals, i.e. 10 / (5 + 5) = 1.
+//OPTIONAL ENHANCEMENT: Add support for handling all cases (uppercase, capitals) for the operations, so that ADD and add and Add (and other combinations) are handled in the same way
+
 const exampleAdditionInput = {
   num1: 3,
   num2: 5,
@@ -64,73 +135,6 @@ const exampleZeroCheck = {
   operation: '/',
 }
 
-const math_options = ['add', '+', 'subtract', '-', 'multiply', '*', 'divide', '/', 'exponentiation', '**', 'modulo', '%']
-
-// The program should have support for these four operations: addition, subtraction, multiplication, and division.
-// The program should accept both the name and the symbol for each possible operation.
-const calculate = function calculate(input) {
-  confirmNumber(input);
-  confirmOperator(input);
-
-  if(input.operation === 'add' || input.operation === '+') {
-    return add(input);
-  }
-  else if(input.operation === 'subtract' || input.operation === '-')  {
-    return subtract(input);
-  }
-  else if(input.operation === 'multiply' || input.operation === '*') {
-    return multiply(input);
-  }
-  else if(input.operation === 'divide' || input.operation === '/') {
-    //TASKED WITH:The program handles divide when attempting to divide by zero
-    if(input.num2 === 0) {
-      console.log('Error: Cannot divide by 0.')
-    }
-    else {
-      return divide(input);
-    }
-  }
-  else if(input.operation === 'modulo' || input.operation === '%') {
-    return modulo(input);
-  }
-}
-
-//TASKED WITH: The program adds numbers accurately with both add and +
-const add = (input) => { console.log(exampleAdditionInput.num1 + exampleAdditionInput.num2); }
-
-//TASKED WITH:The program subtracts numbers accurately with both subtract and -
-const subtract = (input) => { console.log(exampleAdditionInput.num1 - exampleAdditionInput.num2); }
-
-//TASKED WITH:The program adds numbers accurately with both multiply and *
-const multiply = (input) => { console.log(exampleAdditionInput.num1 * exampleAdditionInput.num2); }
-
-//TASKED WITH:The program adds numbers accurately with both divide and /
-const divide = (input) => { console.log(exampleAdditionInput.num1 / exampleAdditionInput.num2); }
-
-//TASKED WITH: The program also needs to handle erroneous operators
-const modulo = (input) => { console.log(exampleAdditionInput.num1 % exampleAdditionInput.num2); }
-
-
-//TASKED WITH:The program handles erroneous input. For example the user might enter clown when asked to enter a number
-//NEEDS: check for given math operator in math operators
-//NEEDS: check for given number in numbers
-
-const confirmOperator = function confirmOperator(input){
-  if(!math_options.includes(input.operation)) {
-    console.log('Error: Please provide a valid operation. \n Provided:', input.operation)
-  }
-}
-
-const confirmNumber = function confirmNumber(input) {
-  if(isNaN(input.num1)) {
-    console.log('Error: Please provide a valid number. \n Provided:', input.num1)
-  }
-  if(isNaN(input.num2)) {
-    console.log('Error: Please provide a valid number. \n Provided:', input.num2)
-  }
-}
-
-
 calculate(exampleAdditionInput)
 calculate(exampleAddition2Input)
 calculate(exampleSubtractionInput)
@@ -142,26 +146,3 @@ calculate(exampleDivide2Input)
 calculate(exampleNumberCheck)
 calculate(exampleOperatorCheck)
 calculate(exampleZeroCheck)
-
-// const numberTest = {
-//   num1: 'dog',
-//   num2: 5,
-//   operation: 'divide',
-// }
-
-// const numberTest2 = {
-//   num1: 'dog',
-//   num2: 5,
-//   operation: 'divide',
-// }
-
-// confirmNumber(numberTest)
-// confirmNumber(numberTest2)
-
-// const operatorTest = {
-//   num1: 5,
-//   num2: 5,
-//   operation: 'seventeen',
-// }
-
-// confirmOperator(operatorTest)
